c3b760e692249678f88176936bdb21d605fa6e83
Anthony Castro <anthony618@gmail.com>
2023-05-09 20:56:52 -0400

    Try adding event listener

--- src/RecipeViewer.tsx
+++ src/RecipeViewer.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function RecipeViewer() {
+  useEffect(() => {
+    const listener = (event: MessageEvent) => {
+      // We only accept messages from ourselves
+      if (event.source !== window) {
+        return;
+      }
+
+      console.log("received message:", event.data);
+    };
+
+    window.addEventListener("message", listener, false);
+
+    return () => {
+      console.log("removing event listener");
+      window.removeEventListener("message", listener);
+    };
+  }, []);
+
   return <h1>Recipe</h1>;
 }
