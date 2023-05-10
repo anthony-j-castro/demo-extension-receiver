d5fdc937db410a50c1f92f7cca4fb05abbffc97d
Anthony Castro <anthony618@gmail.com>
2023-05-09 20:36:54 -0400

    Revert "Try listening for chrome extension"
    This reverts commit 553e700802345f3fd3100bc468a38e01ff5dafc4.

--- src/RecipeViewer.tsx
+++ src/RecipeViewer.tsx
@@ -1,17 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function RecipeViewer() {
-  useEffect(() => {
-    const callback = (request: any, sender: any, sendResponse: any) => {
-      console.log("onMessageExternal", request, sender, sendResponse);
-    };
-
-    chrome?.runtime.onMessageExternal.addListener(callback);
-
-    return () => {
-      chrome?.runtime.onMessageExternal.removeListener(callback);
-    };
-  }, []);
-
   return <h1>Recipe</h1>;
 }
